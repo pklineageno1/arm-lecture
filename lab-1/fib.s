e8e26ab2a9d90e12c200c8c943b9231fc98ceff1
pklineageno1 <pklineageno1@hotmail.com>
2015-09-30 04:22:39 +0800

    revised code

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -9,26 +9,22 @@
 	.type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r3, r4, r5, lr};
+        subs r4, r0, #0;
+        ble .L3;
+      
+        cmp r4, #1;
+        beq .L4;
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+        add r0, r4, #4294967295;
+        bl fibonacci;
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+        mov r5, r0;
+        sub r0, r4, #2;
+        bl fibonacci;
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+        adds r0, r5, r0;
+        pop {r3, r4, r5, pc};        
 
 	@ END CODE MODIFICATION
 .L3:
